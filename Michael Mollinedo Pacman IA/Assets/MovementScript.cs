f6563e246d0963b0c109953a8b92890e28e6bef4
michammp1 <megaman-x4@hotmail.com>
2018-08-15 19:32:52 -0400

    Actualizado

---
+++ Michael Mollinedo Pacman IA/Assets/MovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    
+    UnityEngine.AI.NavMeshAgent agentedeNavegacion;
+    public GameObject destino;
+
+    void Start()
+    {
+        agentedeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agentedeNavegacion.SetDestination(destino.transform.position);
+    }
+
+    void Update()
+    {
+
+    }
+}
